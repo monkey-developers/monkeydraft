dbdbe0328ec0166982a4f519f56ea25cafdda44e
iguoliveira <igor.or042@gmail.com>
2023-03-23 14:11:09 -0300

    feat: get and post methods to markdown

--- server/src/routers/markdown.ts
+++ server/src/routers/markdown.ts
@@ -1,5 +1,26 @@
 import { controller } from "../controller"
+import { db } from "../database"
 
-controller.get("/markdown", async (req, res) => {
-    res.send('sim')
+controller.get("/getMarkdown", async (req, res) => {
+    db.serialize(() => {
+        db.all(
+            "SELECT * FROM Markdown",
+            (error: Error, markdown: Array<{}>) => {
+                if (error) return res.status(500).json({ error, msg: error.message })
+                res.json({ markdown: markdown })
+            }
+        )
+    })
+})
+
+controller.post("/send-markdown", async (req, res) => {
+    db.serialize(() => {
+        db.run(
+            "INSERT INTO Markdown (content, author) VALUES (?,?)",
+            [
+                req.body.content,
+                req.body.author
+            ]
+        )
+    })
 })
